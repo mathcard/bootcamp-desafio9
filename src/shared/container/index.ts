cb430abd2b622b1d55b85d48cddafa7a1652a89f
mathcard <mathcardoso.94@gmail.com>
2020-12-19 18:22:52 -0300

    Finalizando criação das migrations

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,16 @@ import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import OrdersRepository from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
 
 container.registerSingleton<ICustomersRepository>(
-  'CustomerRepository',
+  'CustomersRepository',
   CustomersRepository,
+);
+
+container.registerSingleton<IProductsRepository>(
+  'ProductsRepository',
+  ProductsRepository,
+);
+
+container.registerSingleton<IOrdersRepository>(
+  'CustomersRepository',
+  OrdersRepository,
 );
